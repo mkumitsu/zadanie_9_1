7a51e69d5f9c8dc7225ff2cf450993c6c3e2a6d0
Maciek Bulski <buli@toya.net.pl>
2018-04-08 15:31:10 +0100

    update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 "use strict";
-var a = 4;
+var a = 2;
 var h = 5;
-var area = getTriangleArea(a, h);
 
 function getTriangleArea(a, h) {
     if ((a <= 0) || (h <= 0)) {
         alert("Nieprawidłowe dane");
     } else {
         var triangleArea = a * h / 2;
-        return alert("Pole trójkąta wynosi: " + triangleArea);     
+        return triangleArea;
     }
 }
+var area = getTriangleArea(a, h);
