a79ccfc3c766ac0ce31a4e7c4756ba62c9bd2698
Maciek Bulski <fragmast3r@gmail.com>
2018-04-09 23:08:46 +0100

    fixed function return in script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ var triangle3Area = getTriangleArea(-30, 35);
 function getTriangleArea(a, h) {
 
     if ((a <= 0) || (h <= 0)) {
-        alert("Nieprawidłowe dane");
+        return alert("Nieprawidłowe dane");
     } else {
         return a * h / 2;
     }
