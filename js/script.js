be104a1017b8bcc83f55540e220734d972ddc23a
Maciek Bulski <fragmast3r@gmail.com>
2018-04-09 18:49:26 +0100

    shortened the code in script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
 "use strict";
 var triangle1Area = getTriangleArea(10, 15);
 var triangle2Area = getTriangleArea(20, 25);
-var triangle3Area = getTriangleArea(30, 35);
+var triangle3Area = getTriangleArea(-30, 35);
 
 function getTriangleArea(a, h) {
 
-  if ((a <= 0) || (h <= 0)) {
+    if ((a <= 0) || (h <= 0)) {
         alert("Nieprawidłowe dane");
     } else {
-        const triangleAreaEq = a * h / 2;
-        return triangleAreaEq;
+        return a * h / 2;
     }
 }
 console.log(getTriangleArea(10, 6));
-console.log(triangle1Area);
+console.log(triangle3Area);
